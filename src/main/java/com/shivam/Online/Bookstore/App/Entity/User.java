d5af7658efb15cbba86f67d2a5c7aa457f2a21a4
kushwahshivam <shivamkushwah97522@gmail.com>
2025-10-26 15:30:10 +0530

    User class Added

---
+++ src/main/java/com/shivam/Online/Bookstore/App/Entity/User.java
@@ -0,0 +1,23 @@
+package com.shivam.Online.Bookstore.App.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @NotNull
+    private String password;
+}
